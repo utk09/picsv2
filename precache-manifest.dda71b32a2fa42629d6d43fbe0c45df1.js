097b751bb73d2f6b60becd673277a37e45c8fea9
utkarsh <utkarsh.tiwari@somaiya.edu>
2019-10-31 11:40:24 +0530

    Updates

---
+++ precache-manifest.dda71b32a2fa42629d6d43fbe0c45df1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4700f525d1ec58e349211b2ed04cbf1",
+    "url": "/picsv2/index.html"
+  },
+  {
+    "revision": "6e235d63ac796792894d",
+    "url": "/picsv2/static/css/main.2643ea90.chunk.css"
+  },
+  {
+    "revision": "19d12416d1b8022a4675",
+    "url": "/picsv2/static/js/2.14f666b4.chunk.js"
+  },
+  {
+    "revision": "6e235d63ac796792894d",
+    "url": "/picsv2/static/js/main.6dbf7476.chunk.js"
+  },
+  {
+    "revision": "2ff582a8e5b3e9875c16",
+    "url": "/picsv2/static/js/runtime-main.db155066.js"
+  }
+]);
